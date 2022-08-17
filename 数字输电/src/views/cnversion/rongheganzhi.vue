addff82543bb82beabc274bc026e1accbe7e3730
peppababy7 <hunter4n@gmail.com>
2022-08-17 15:58:49 +0800

    数字输电

--- 数字输电/src/views/cnversion/rongheganzhi.vue
+++ 数字输电/src/views/cnversion/rongheganzhi.vue
@@ -20,7 +20,7 @@
         src="../../assets/videos/en/04融合感知.mp4"
     ></video>
     <img
-      class="home_btn"
+      class="home_btn rb0-fixed"
       src="../../assets/imgs/home.png"
       @click="$router.push({ path:'/', query: $route.query})"
     />
